fb5ab759b4e41415514aa4625d7c879b72b55b22
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-25 15:04:16 +0200

    ajout getenv()

--- environnement.php
+++ environnement.php
@@ -3,5 +3,13 @@
 
     start_page('Environnement');
 
+//Correspond à l’adresse IP de celui qui visualise la page.
+    echo getenv('REMOTE_ADDR');
+//Correspond à l’adresse IP du serveur.
+    echo getenv('HTTP_HOST');
+//Correspond au logiciel serveur Web utilisé.
+    echo getenv('SERVER_SOFTWARE');
 
+
+    phpinfo();
 ?>
