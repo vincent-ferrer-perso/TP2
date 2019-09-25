d6b720f1acc5efafd0aa33ad5056936bce23c75b
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-25 16:13:19 +0200

    fonctionnnemnt lien dormulaire data-processing.php

--- data-processing.php
+++ data-processing.php
@@ -1,11 +1,15 @@
 <?php
 
-    $id = $POST['id'];
-
-
-
+    $id = $_POST['id'];
+    $civilite = $_POST['civilite'];
+    $mail = $_POST['e-mail'];
+    $mdp = $_POST['mdp'];
+    $mdpV = $_POST['mdpV'];
+    $pays = $_POST['pays'];
+    $action = $_POST['action'];
 
     if($action=='mailer'){
+        echo 'OK';
     }else{
-        echo'<br/><strong>Boutonnongéré!</strong><br/>';
+        echo'<br/><strong>Bouton non géré!</strong><br/>';
     }
--- formulaire.php
+++ formulaire.php
@@ -4,25 +4,25 @@
                 <input type="text" id="Id" required placeholder="Ex: foufou32" name="id" ><br>
                 
                 <label>Civilité : </label><br>
-                <label for="man">Homme<input id="man" type="radio" value="man"></label>
-                <label  for="woman">Femme<input id="woman" type="radio" value="woman"></label><br>
+                <label for="man">Homme<input id="man" type="radio" value="man" name="civilite"></label>
+                <label  for="woman">Femme<input id="woman" type="radio" value="woman" name="civilite"></label><br>
                 
     
                 <label for="mail">Mail : </label><br>
-                <input type="text" id="mail" required placeholder="Ex: toutou@mail.fr"><br>
+                <input type="text" id="mail" name="e-mail" required placeholder="Ex: toutou@mail.fr"><br>
 
 
                
                 <label for="mdp">Mot de passe :</label><br>
-                <input  type="password" id="mdp"><br>
+                <input  type="password" id="mdp" name="mdp"><br>
 
                 <label for="mdpV">Vérification de mot de passe :</label><br>
-                <input  type="password" id="mdpV"><br>
+                <input  type="password" id="mdpV" name="mdpV"><br>
                 
                 
                 <label for="country">Country</label>     
         
-                    <select id="country" name="country" class="form-control">
+                    <select id="country" name="pays" class="form-control">
                         <option value="Afghanistan">Afghanistan</option>
                         <option value="Åland Islands">Åland Islands</option>
                         <option value="Albania">Albania</option>
