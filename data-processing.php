20aeb551535e7e7dfd1a863647800815b65fee9d
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 09:16:13 +0200

    envoi requete au serveur et affichage

--- data-processing.php
+++ data-processing.php
@@ -22,3 +22,27 @@
     }else{
         echo'<br/><strong>Bouton non géré!</strong><br/>';
     }
+
+
+
+    $dbLink=mysqli_connect('mysql-vincentferrer.alwaysdata.net',173776,'Vf36@13300')
+    or die('Erreur de connexion au serveur: ' .mysqli_connect_error());
+
+
+    mysqli_select_db($dbLink,'vincentferrer_tp_php')
+    or die('Erreur dans la sélection de la base:'.mysqli_error($dbLink));
+
+    $query = 'INSERT INTO user(NomUtilisateur,Civilite,Mail,MotDePasse,Pays,DateInscription) 
+                VALUES (\''.$id.'\',\''.$civilite.'\',\''.$mail.'\',\''.$mdp.'\',\''.$pays.')';
+
+
+    if(!($dbResult=mysqli_query($dbLink, $query))){
+        echo'Erreur dans requête<br/>';               //Affiche le type d'erreur.
+        echo'Erreur: '.mysqli_error($dbLink).'<br/>'; //Affiche la requête envoyée.
+        echo'Requête: '.$query.'<br/>';
+        exit();
+    }else{
+        echo 'Bonjour '.$id.'<br/>';
+        echo 'Votre inscription à bien été enregistrée.'.'<br/>';
+        echo '<br/><br/>';
+    }
