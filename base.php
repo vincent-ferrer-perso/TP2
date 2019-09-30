d17f09d0c8423e5674664f320afd3e459bba7502
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 08:41:06 +0200

    Envoi requete a la base de donnée 2

--- base.php
+++ base.php
@@ -1,5 +1,5 @@
 <?php
-    $dbLink=mysqli_connect(mysql-vincentferrer.alwaysdata.net,173776,motdepasseMySQL)
+    $dbLink=mysqli_connect(mysql-vincentferrer.alwaysdata.net,173776,'Vf36@13300')
     or die('Erreur de connexion au serveur: ' .mysqli_connect_error());
 
 
