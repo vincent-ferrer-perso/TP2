535a03fa7b9f486457ab786b4ef24b62be2e5da7
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 10:07:30 +0200

    test lien google 2

--- test-pass.php
+++ test-pass.php
@@ -1,11 +1,7 @@
 <?php
     include 'utils.inc.php';
     start_page('login');
-    lienBD();
 
-
-    $id  = $_POST['id'      ];
-    $mdp = password_hash($_POST['mdp'],PASSWORD_BCRYPT);
 
         header('www.google.fr');
 
