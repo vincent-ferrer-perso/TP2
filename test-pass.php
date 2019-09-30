b367f16efba6af0650a0ca9ee91ed24b4d3d0678
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 10:18:50 +0200

    test lien google avec verif login

--- test-pass.php
+++ test-pass.php
@@ -1,3 +1,13 @@
 <?php
-header('Location: https://www.google.fr');
+    include 'utils.inc.php';
+    start_page('login');
+    lienBD();
+
+
+    $id  = $_POST['id'];
+    $mdp = password_hash($_POST['mdp'],PASSWORD_BCRYPT);
+
+    if(!('SELECT NomUtilisateur FROM user WHERE NomUtilisateur = \''.$id.'\'')) {
+        header('Location: https://www.google.fr');
+    }
 
