c466325270f71dc719ee826213e1b61e7579996c
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 11:42:31 +0200

    test verif login 29

--- test-pass.php
+++ test-pass.php
@@ -19,7 +19,7 @@
         exit();
     }
 
-    if($dbResult) {
+    if(mysqli_fetch_assoc($dbResult) != NULL) {
         while ($dbRow = mysqli_fetch_assoc($dbResult)) {
             if ($Login != $dbRow['NomUtilisateur']) {
                 echo 'Pas bon';
