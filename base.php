14ae2a4decb1c6fa5e9937ec873c908a3980e5fb
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 08:53:08 +0200

    Envoi requete a la base de donnée 7

--- base.php
+++ base.php
@@ -10,8 +10,18 @@
 
     $query='SELECT NomUtilisateur,Mail,DateInscription FROM user';
 
-    if(!($dbResult=mysqli_query($dbLink, $query))){echo'Erreur de requête<br/>';//Affiche le type d'erreur
+    if(!($dbResult=mysqli_query($dbLink, $query))){
+        echo'Erreur de requête<br/>';//Affiche le type d'erreur
         echo 'Erreur: '.mysqli_error($dbLink).'<br/>';//Affiche la requête envoyée.
         echo 'Requête: '.$query.'<br/>';
         exit();
+    }
+
+
+    while($dbRow=mysqli_fetch_assoc($dbResult)){
+        echo $dbRow['NomUtilisateur' ].'<br/>';
+        echo $dbRow['Mail'           ].'<br/>';
+        echo $dbRow['DateInscription'].'<br/>';
+        echo '<br/><br/>';
+
     }
