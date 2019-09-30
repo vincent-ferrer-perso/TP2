341a33eea80641deaedc430dbfa5d85805bb37d1
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 08:38:14 +0200

    Envoi requete a la base de donnée

--- base.php
+++ base.php
@@ -1,3 +1,17 @@
 <?php
     $dbLink=mysqli_connect(mysql-vincentferrer.alwaysdata.net,173776,motdepasseMySQL)
     or die('Erreur de connexion au serveur: ' .mysqli_connect_error());
+
+
+    mysqli_select_db($dbLink,dbBd)
+    or die('Erreur dans la sélection de la base:'.mysqli_error($dbLink));
+
+
+
+    $query='SELECT id,email,date FROM user';
+
+    if(!($dbResult=mysqli_query($dbLink, $query))){echo'Erreurderequête<br/>';//Affiche le type d'erreur
+        echo 'Erreur: '.mysqli_error($dbLink).'<br/>';//Affiche la requête envoyée.
+        echo 'Requête: '.$query.'<br/>';
+        exit();
+    }
