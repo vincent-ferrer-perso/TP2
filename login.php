7040ce86517f2052905c4157b38bdff604f901e6
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-09-30 09:47:50 +0200

    formulaire login

--- login.php
+++ login.php
@@ -1,2 +1,10 @@
 <?php
-    echo 'test';
+    echo '<form method="POST" action="data-processing.php">
+                <label for="Id">Identifiant : </label><br>
+                <input type="text" id="Id" required placeholder="Ex: foufou32" name="id" ><br>
+
+                <label for="mdp">Mot de passe :</label><br>
+                <input  type="password" id="mdp" name="mdp"><br>           
+                    
+                <label for=soumettre><input id="soumettre" type="submit" name="action" value="mailer"></label>
+            </form>';
