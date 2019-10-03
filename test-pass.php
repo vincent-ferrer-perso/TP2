b59afc3ac8483e9469089e3f46fb96d478c47c45
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-03 13:16:18 +0200

    verifiacation connexion pour acces page

--- test-pass.php
+++ test-pass.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+
     $dbLink=mysqli_connect('mysql-vincentferrer.alwaysdata.net',173776,'Vf36@13300')
     or die('Erreur de connexion au serveur: ' .mysqli_connect_error());
 
