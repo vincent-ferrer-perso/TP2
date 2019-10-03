dc09d3c2f11e0809bdab7d5450f8257761eea715
vincent-ferrer <vincent.ferrer@etu.univ-amu.fr>
2019-10-03 13:10:43 +0200

    verifiacation connexion pour acces page

--- Accueil.php
+++ Accueil.php
@@ -1,3 +1,3 @@
 <li><a href="formulaire.php">Formulaire inscription</a></li>
 <li><a href='login.php?step=LOGIN'>Login</a></li>
-<li><a href='logOK'>Login</a></li>
+<li><a href='logOK'>LoginOK</a></li>
